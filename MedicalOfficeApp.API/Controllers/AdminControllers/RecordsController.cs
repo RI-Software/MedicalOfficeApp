ecdd481da1a2f2343838aac0caa35823388d95fa
Ignatella <ignatellasecond@gmail.com>
2020-09-24 19:38:06 +0200

    Add RecordController to seed records from DB .API

---
+++ MedicalOfficeApp.API/Controllers/AdminControllers/RecordsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicalOfficeApp.API.Core;
+using MedicalOfficeApp.API.Data;
+using MedicalOfficeApp.API.Data.Repositories;
+using MedicalOfficeApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace MedicalOfficeApp.API.Controllers.AdminControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        private readonly IRecordRepository repo;
+        private readonly IOptions<DateRecordCollection> recordsInMemory;
+
+        public RecordsController(
+             IRecordRepository repo,
+             IOptions<DateRecordCollection> recordsInMemory)
+        {
+            this.repo = repo;
+            this.recordsInMemory = recordsInMemory;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> Records([FromQuery]RecordParams recordParams)
+        {
+            return Ok(await ApiResult<DbRecord>.CreateAsync(
+                repo.GetRecords(),
+                recordParams.PageIndex,
+                recordParams.PageSize,
+                recordParams.SortColumns,
+                recordParams.SortOrder));
+        }
+    }
+}
---
+++ MedicalOfficeApp.API/Dtos/AdminDtos/RecordParams.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace MedicalOfficeApp.API.Models
+{
+    public class RecordParams
+    {
+        private int pageSize = 10;
+
+        private int maxPageSize = 50;
+
+        private int minPageSize = 1;
+
+        public int PageIndex { get; set; } = 0;
+
+        public int PageSize {
+            get =>
+                pageSize;
+
+            set
+            {
+                if (value > maxPageSize)
+                {
+                    pageSize = maxPageSize;
+                    return;
+                }
+
+                if (value < minPageSize)
+                {
+                    pageSize = minPageSize;
+                    return;
+                }
+
+                pageSize = value;
+            }
+        }
+
+        public List<string> SortColumns { get; set; } = null;
+
+        public string? SortOrder { get; set; } = null;
+    }
+}
